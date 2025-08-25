06897f79720e0a26dabbc9d901c60d0f2ab39a6d
Utkarsh Tripathi <utkarshtripathi.rpt@gmail.com>
2025-08-25 09:27:04 +0530

    nTo1.py

--- 2.py
+++
@@ -1,4 +0,0 @@
-# 2
-N = int(input("Enter a positive integer N: "))
-for i in range(N, 0, -1):
-    print(i, end=' ')
---
+++ nTo1.py
@@ -0,0 +1,4 @@
+#print numbers from n to 1
+n = int(input("Enter a positive integer N: "))
+for i in range(n, 0, -1):
+    print(i, end=' ')
