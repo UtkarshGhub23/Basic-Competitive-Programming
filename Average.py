fc55332cb0d954527a12af1e6c48a1c6a54bc7e6
Utkarsh Tripathi <utkarshtripathi.rpt@gmail.com>
2025-08-26 11:47:32 +0530

    Average.py

--- Average.py
+++ Average.py
@@ -1,16 +1,13 @@
-# Given 5 number A,B,C,D,E as input. print the average of these 5 number ?
+#print the average of 5 numbers
 
-# Input 5 numbers
 A = float(input("Enter number A: "))
 B = float(input("Enter number B: "))
 C = float(input("Enter number C: "))
 D = float(input("Enter number D: "))
 E = float(input("Enter number E: "))
 
-# Calculate average
 average = (A + B + C + D + E) / 5
 
-# Print result
 print("The average is:", average)
 
 
