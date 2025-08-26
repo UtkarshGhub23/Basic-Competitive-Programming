e41bfdb13e3ca0877df9ee2daaf408ad7562a019
Utkarsh Tripathi <utkarshtripathi.rpt@gmail.com>
2025-08-26 11:42:10 +0530

    sumOdd.py

--- 7.py
+++
@@ -1,6 +0,0 @@
-# 7
-A = int(input("Enter an integer A: "))
-sum = 0
-for i in range(1, A+1, 2):
-    sum += i
-print(sum)
---
+++ sumOdd.py
@@ -0,0 +1,6 @@
+#print the sum of all odd numbers between 1 to n
+a = int(input("Enter an integer a: "))
+sum = 0
+for i in range(1, A+1, 2):
+    sum += i
+print(sum)
